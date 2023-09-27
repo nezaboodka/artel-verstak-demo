86caae7e242fd53a3a3635ee925acca47dbddaad
Yury Chetyrko <ychetyrko@gmail.com>
2023-09-27 11:34:27 +0300

    Подготовка пакета Верстак

---
+++ .сборка/application/приложение/верстак/index.mjs
@@ -0,0 +1,16 @@
+"use strict";
+export class __artel__type__3 {
+  data;
+  window;
+  constructor() {
+    let __artel__promises = [];
+  }
+}
+export class __artel__type__4 {
+  dataList;
+  window;
+  activeData;
+  constructor() {
+    let __artel__promises = [];
+  }
+}
---
+++ .сборка/launch.mjs
@@ -0,0 +1,12 @@
+"use strict";
+import { __artel__ctor__0 } from "./packages/артель/index.mjs";
+import { __artel__ctor__1 } from "./packages/артель/js/index.mjs";
+import { __artel__entry_point__2 } from "./application/приложение/index.mjs";
+export async function __artel__main() {
+  __artel__ctor__0();
+  __artel__ctor__1();
+  __artel__entry_point__2();
+}
+(async () => {
+  await __artel__main();
+})();
