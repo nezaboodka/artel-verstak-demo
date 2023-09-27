25cd9a62d25d970c6f8c136ba614bf296848de10
Yury Chetyrko <ychetyrko@gmail.com>
2023-09-27 12:35:04 +0300

    Добавлен vite

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import * as path from 'path'
+
+const outDir = path.resolve(__dirname, './сборка/приложение')
+
+function noTreeShakeForFilesWithEvalPlugin() {
+  return {
+    name: 'no-tree-shake-for-files-with-eval',
+    transform(code) {
+      if (code.indexOf('eval(') >= 0)
+        return { moduleSideEffects: 'no-treeshake' }
+    }
+  }
+}
+
+export default defineConfig({
+  base: "./",
+  server: {
+    port: 1234,
+    hmr: false,
+  },
+  build: {
+    target: 'es2016',
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      plugins: [noTreeShakeForFilesWithEvalPlugin()]
+    }
+  },
+  resolve: {
+  },
+  esbuild: {
+    target: 'es2016',
+  },
+})
