c532ee5a9edee3ad1c0b52a497b7b8e97c7f8000
Yury Chetyrko <ychetyrko@gmail.com>
2024-02-11 17:18:30 +0300

    R&V 0.24.201

--- Библиотека/Реактроник/core/RxNode.d.ts
+++ Библиотека/Реактроник/core/RxNode.d.ts
@@ -1,14 +1,14 @@
 import { LoggingOptions } from "../Logging.js";
 import { MergeListReader, MergedItem } from "../util/MergeList.js";
 import { MemberOptions } from "../Options.js";
-export type Delegate<T> = (element: T, base: () => void) => void;
-export type SimpleDelegate<T = unknown, R = void> = (element: T) => R;
+export type Delegate<T> = (el: T, basis: () => void) => void;
+export type SimpleDelegate<T = unknown, R = void> = (el: T) => R;
 export declare enum Mode {
     default = 0,
     independentUpdate = 1,
     manualMount = 2
 }
-export declare const enum Priority {
+export declare enum Priority {
     realtime = 0,
     normal = 1,
     background = 2
@@ -35,7 +35,7 @@ export declare abstract class RxNode<E = unknown> {
     static readonly longFrameDuration = 300;
     static currentUpdatePriority: Priority;
     static frameDuration: number;
-    static acquire<E = void>(driver: RxNodeDriver<E>, declaration?: RxNodeDecl<E>, preset?: RxNodeDecl<E>): RxNode<E>;
+    static declare<E = void>(driver: RxNodeDriver<E>, declaration?: RxNodeDecl<E>, preset?: RxNodeDecl<E>): RxNode<E>;
     static get isFirstUpdate(): boolean;
     static get key(): string;
     static get stamp(): number;
@@ -45,7 +45,7 @@ export declare abstract class RxNode<E = unknown> {
     static get childrenShuffling(): boolean;
     static set childrenShuffling(value: boolean);
     static triggerUpdate(node: RxNode<any>, triggers: unknown): void;
-    static triggerFinalize(node: RxNode<any>): void;
+    static triggerDeactivation(node: RxNode<any>): void;
     static updateNestedNodesThenDo(action: (error: unknown) => void): void;
     static markAsMounted(node: RxNode<any>, yes: boolean): void;
     static findMatchingHost<E = unknown, R = unknown>(node: RxNode<E>, match: SimpleDelegate<RxNode<E>, boolean>): RxNode<R> | undefined;
@@ -55,23 +55,23 @@ export declare abstract class RxNode<E = unknown> {
     static setDefaultLoggingOptions(logging?: LoggingOptions): void;
 }
 export type RxNodeDecl<E = unknown> = {
-    preset?: RxNodeDecl<E>;
+    content?: Delegate<E>;
     key?: string;
     mode?: Mode;
+    activation?: Delegate<E>;
+    deactivation?: Delegate<E>;
     triggers?: unknown;
-    initialize?: Delegate<E>;
-    update?: Delegate<E>;
-    finalize?: Delegate<E>;
+    preset?: RxNodeDecl<E>;
 };
 export type RxNodeDriver<E = unknown> = {
     readonly name: string;
     readonly isPartitionSeparator: boolean;
     readonly predefine?: SimpleDelegate<E>;
     allocate(node: RxNode<E>): E;
-    initialize(node: RxNode<E>): void;
+    activate(node: RxNode<E>): void;
     mount(node: RxNode<E>): void;
     update(node: RxNode<E>): void | Promise<void>;
-    finalize(node: RxNode<E>, isLeader: boolean): boolean;
+    deactivate(node: RxNode<E>, isLeader: boolean): boolean;
 };
 export type RxNodeContext<T extends Object = Object> = {
     value: T;
@@ -82,10 +82,10 @@ export declare abstract class BaseDriver<E = unknown> implements RxNodeDriver<E>
     readonly predefine?: SimpleDelegate<E, void> | undefined;
     constructor(name: string, isPartitionSeparator: boolean, predefine?: SimpleDelegate<E, void> | undefined);
     abstract allocate(node: RxNode<E>): E;
-    initialize(node: RxNode<E>): void;
+    activate(node: RxNode<E>): void;
     mount(node: RxNode<E>): void;
     update(node: RxNode<E>): void | Promise<void>;
-    finalize(node: RxNode<E>, isLeader: boolean): boolean;
+    deactivate(node: RxNode<E>, isLeader: boolean): boolean;
 }
 export declare class RxNodeVariable<T extends Object = Object> {
     readonly defaultValue: T | undefined;
